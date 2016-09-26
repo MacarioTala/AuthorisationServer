bfa8d7c2d8e3e7fb8859640d36472c01af1aee05
MacarioTala <vhatriard@Gmail.com>
2016-09-25 23:49:53 -0700

    Adding OauthDemo

---
+++ OAuthDemo/OAuthDemo.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OAuthDemo;
+using OAuthDemo.Controllers;
+
+namespace OAuthDemo.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
